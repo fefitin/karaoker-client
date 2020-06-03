98b351b25120c489810367639310ac07ac68f914
Federico García <fede@divinodi.com>
2020-06-02 23:12:01 -0300

    Google Analytics

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Karaoker from './components/Karaoker';
 import './assets/global.scss';
+import ReactGA from 'react-ga';
+ReactGA.initialize(process.env.REACT_APP_ANALYTICS_UA);
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
   <React.StrictMode>
